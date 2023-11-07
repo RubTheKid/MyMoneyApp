e7899ac09eaa7214447574ce5c2316d48030ca88
RubTheKid <jadercardoso@tuta.io>
2023-11-07 18:41:07 -0300

    apllying redux

--- frontend/dashboard/dashboard.jsx
+++ frontend/dashboard/dashboard.jsx
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
 
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { getSummary } from './dashboardActions'
 import ContentHeader from '../src/common/template/contentHeader'
 import Content from '../src/common/template/content'
+import ValueBox from '../src/common/widget/valueBox'
 
 class Dashboard extends Component {
+
+    componentWillMount(){
+        this.props.getSummary()
+    }
+
     render() {
+        const { credit, debt } = this.props.summary
 
         return(
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    Dashboard
+                    <ValueBox cols='12 4' color='green' icon='bank' value={`R$ ${credit}`} text="Total de Créditos" />
+                    <ValueBox cols='12 4' color='red' icon='credit-card' value={`R$ ${debt}`} text="Total de Débitos" />
+                    <ValueBox cols='12 4' color='blue' icon='money' value={`R$ ${credit - debt}`} text="Valor Consolidado" />
                 </Content>
             </div>
         )
     }
 }
 
-export default Dashboard
+const mapStateToProps = state => ({summary: state.dashboard.summary})
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
