817da4f607534a4a45d24c49683731d6fc231c12
RubTheKid <jadercardoso@tuta.io>
2023-11-16 13:13:54 -0300

    update

--- frontend/billingCycle/billingCycle.jsx
+++ frontend/billingCycle/billingCycle.jsx
@@ -1,7 +1,32 @@
-import React from "react"
+import React, { Component } from "react"
 
-export default props => (
-    <div>
-        <h1>Ciclo de pagamentos</h1>
-    </div>
-)
+import ContentHeader from '../src/common/template/contentHeader'
+import Content from '../src/common/template/content'
+import Tabs from "../src/common/tab/tabs"
+import TabsHeader from "../src/common/tab/tabsHeader"
+import TabsContent from "../src/common/tab/tabsContent"
+
+import TabHeader from "../src/common/tab/tabHeader"
+
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title="Ciclos de Pagamentos" small='cadastro' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent></TabsContent>    
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillingCycle
--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,15 +1,14 @@
 import React from "react"
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
-//import Dashboard from '../../dashboard/dashboard'
-import Dashboard from '../../dashboard2/Dashboard2'
+import Dashboard from '../../dashboard/dashboard'
 import BillingCycle from '../../billingCycle/billingCycle'
-import Dashboard2 from '../../dashboard2/dashboard2';
+
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard2} />
-        <Route path='/billingCycles' component={BillingCycle} />
+        <Route path='/' component={Dashboard} />
+        <Route path='/BillingCycles' component={BillingCycle} />
         <Redirect from='*' to='/' />
     </Router>
 )
