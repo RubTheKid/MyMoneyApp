9a9a5fca25d0dd88a8c6adcdceb3ddd8577c311f
RubTheKid <jadercardoso@tuta.io>
2023-11-28 14:17:59 -0300

    fixes

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,14 +1,16 @@
 import React from "react"
-import { Router, Route, Redirect, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
 import Dashboard from '../../dashboard/dashboard'
 import BillingCycle from '../../billingCycle/billingCycle'
-
+import App from './app'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
-        <Route path='/BillingCycles' component={BillingCycle} />
+        <Route path='/' component={App}>
+            <IndexRoute component={Dashboard} />
+            <Route path='/BillingCycles' component={BillingCycle} />
+        </Route>
         <Redirect from='*' to='/' />
     </Router>
 )
